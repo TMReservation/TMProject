9b2307858993b9ea406c4e4db70172d8d48f1dc7
traduce <dgagan7@gmail.com>
2014-11-17 21:09:14 -0600

    Change in setting file for the project

---
+++ TMChecking/TMChecking-web/src/main/java/com/mum/setting/SettingBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mum.setting;
+
+import com.tm.entities.Setting;
+import com.tm.setting.SettingRoleLocal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author sunil
+ */
+@ManagedBean(name = "settingBean")
+@SessionScoped
+public class SettingBean {
+    @EJB
+    private SettingRoleLocal settingRoleLocal;
+    List<Setting> settings=new ArrayList<>();
+    private int currentSettingIndex;
+    private Setting setting;
+    
+    public String showUserList(){
+        System.out.println("IN SETTING BEAN");
+        
+        settings=settingRoleLocal.getSettingRole();
+       // System.out.println("Teacher size is "+settings.size());
+        return "setting";
+    }
+    
+     public void store() {
+        settings.set(currentSettingIndex, setting);
+    }
+
+    public List<Setting> getSettings() {
+        return settings;
+    }
+
+    public void setSettings(List<Setting> settings) {
+        this.settings = settings;
+    }
+
+    public int getCurrentSettingIndex() {
+        return currentSettingIndex;
+    }
+
+    public void setCurrentSettingIndex(int currentSettingIndex) {
+        this.currentSettingIndex = currentSettingIndex;
+    }
+
+    public Setting getSetting() {
+        System.out.println("HIT SETTING BEAN "+setting.getId());
+        return setting;
+    }
+
+    public void setSetting(Setting setting) {
+        this.setting = setting;
+    }
+    
+    
+}
