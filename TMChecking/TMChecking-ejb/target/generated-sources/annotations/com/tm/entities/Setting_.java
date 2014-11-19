9be09c07ab3a4c32f7e97c6abd8bd180d06daf64
unknown <navinkumarkarn@hotmail.com>
2014-11-19 12:22:54 -0600

    commit here

---
+++ TMChecking/TMChecking-ejb/target/generated-sources/annotations/com/tm/entities/Setting_.java
@@ -0,0 +1,15 @@
+package com.tm.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-19T00:42:38")
+@StaticMetamodel(Setting.class)
+public class Setting_ { 
+
+    public static volatile SingularAttribute<Setting, Integer> teacherId;
+    public static volatile SingularAttribute<Setting, String> role;
+    public static volatile SingularAttribute<Setting, Integer> id;
+
+}
